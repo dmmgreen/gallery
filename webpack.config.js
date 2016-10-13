e29bfc3540b1c56320307bd9ff7239abc8540486
dmmgreen <dmmgreen@qq.com>
2016-10-13 17:11:17 +0800

    重新build
    修改webpack limit

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports={
             {
                 test:/\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
                 exclude:/node_modules/,
-                loader:'url-loader?limit=1024'
+                loader:'url-loader?limit=8192'
             }
         ]
     }
